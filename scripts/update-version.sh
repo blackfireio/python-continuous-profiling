26b9e2a6844541f4c8c2cef260ff6998a1c5f5f8
Sümer Cip <sumer.cip@platform.sh>
2023-11-22 13:47:52 +0300

    fix review comments
    remove REV_COUNT
    
    print commands

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -2,31 +2,22 @@
 
 set -eux
 
-#!/bin/bash
-
-set -eu
-
 git fetch --tags --force
 
 PREV_TAG=`git describe --tags --abbrev=0`
-REV_COUNT=""
 VERSION=""
 
 # when executing this script with a git tag defined by buildkite, we are releasing to production
 if [[ ${BUILDKITE_TAG:-""} != "" ]]; then
     VERSION="$BUILDKITE_TAG"
 # when executing this script on the master branch, we are releasing internal versions
-elif [[ ${BUILDKITE_BRANCH:-} != "master" ]]; then
+elif [[ ${BUILDKITE_BRANCH:-} == "master" ]]; then
     VERSION=${PREV_TAG}
 
      # if there are multiple revisions, append that revision number
     _REV_COUNT=$(git rev-list "${PREV_TAG}"..HEAD --count 2>/dev/null) || true
     if [[ ! -z "${_REV_COUNT:-}" ]]; then
-        REV_COUNT=${_REV_COUNT}
-    fi
-
-    if [[ ${REV_COUNT} != "" ]]; then
-        VERSION="${PREV_TAG}+internal.${REV_COUNT}"
+        VERSION="${PREV_TAG}+internal.${_REV_COUNT}"
     fi
 fi
 
