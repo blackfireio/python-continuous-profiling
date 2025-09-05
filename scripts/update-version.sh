7b1b1d8057944698f4ef4f7ea299b9a27a460ef3
Julien Jean <julien.jean@platform.sh>
2025-09-05 16:46:54 +0200

    [python-conprof] fix github staging pipeline

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -6,8 +6,23 @@ git fetch --tags --force
 PREV_TAG=`git describe --tags --abbrev=0`
 VERSION=""
 
+echo "Previous tag is ${PREV_TAG}"
+
+if [[ ${RELEASE_VERSION:-""} != ""]]; then
+    if [[ ${RELEASE_VERSION} == "master" ]]; then
+        VERSION=${PREV_TAG}
+
+        # if there are multiple revisions, append that revision number
+        _REV_COUNT=$(git rev-list "${PREV_TAG}"..HEAD --count 2>/dev/null) || true
+        echo "REV_COUNT is ${_REV_COUNT}"
+        if [[ ! -z "${_REV_COUNT:-}" ]]; then
+            VERSION="${PREV_TAG}+internal.${_REV_COUNT}"
+        fi
+    else
+        VERSION="${RELEASE_VERSION}"
+    fi
 # when executing this script with a git tag defined by buildkite, we are releasing to production
-if [[ ${BUILDKITE_TAG:-""} != "" ]]; then
+elif [[ ${BUILDKITE_TAG:-""} != "" ]]; then
     VERSION="$BUILDKITE_TAG"
 # when executing this script on the master/staging branch, we are releasing internal versions
 elif [[ ${BUILDKITE_BRANCH:-} == "master" || ${BUILDKITE_BRANCH:-} == "staging" ]]; then
@@ -20,7 +35,8 @@ elif [[ ${BUILDKITE_BRANCH:-} == "master" || ${BUILDKITE_BRANCH:-} == "staging" 
     fi
 fi
 
+echo "VERSION is ${VERSION}"
+
 # write the generated version back to VERSION file which will be used in generation
 # of binaries
 echo -n "__version__ = \"${VERSION}\"" > ./blackfire_conprof/version.py
-
