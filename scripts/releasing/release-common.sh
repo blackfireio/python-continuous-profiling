e0ace1f62a9e9dff9dde496c4844537eff7a7221
Julien Jean <julien.jean@platform.sh>
2025-09-08 15:34:17 +0200

    [python-conprof] also publish package on test.pypi.com when releasing staging

--- scripts/releasing/release-common.sh
+++ scripts/releasing/release-common.sh
@@ -2,17 +2,10 @@
 
 set -eu
 
-# This flag toggles the release destination to testing.pypi.org if set to true
-DEPLOY_TESTPYPI=false
-
 # a TWINE_PASSWORD environment varialbe is required for it to work.
 function release()
 {
     wheel_path="$1"
-
-    if [ "$DEPLOY_TESTPYPI" = true ] ; then
-        export TWINE_REPOSITORY_URL="https://test.pypi.org/legacy/"
-    fi
 
     twine upload $wheel_path/*.whl --verbose
 }
