010bd409490d292050c3b38190f6e7dd3d872f6e
Julien Jean <julien.jean@platform.sh>
2026-02-03 17:32:42 +0000

    [buildkite] remove buildkite refs in release scripts

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -21,18 +21,6 @@ if [[ ${RELEASE_VERSION:-""} != "" ]]; then
     else
         VERSION="${RELEASE_VERSION}"
     fi
-# when executing this script with a git tag defined by buildkite, we are releasing to production
-elif [[ ${BUILDKITE_TAG:-""} != "" ]]; then
-    VERSION="$BUILDKITE_TAG"
-# when executing this script on the master/staging branch, we are releasing internal versions
-elif [[ ${BUILDKITE_BRANCH:-} == "master" || ${BUILDKITE_BRANCH:-} == "staging" ]]; then
-    VERSION=${PREV_TAG}
-
-     # if there are multiple revisions, append that revision number
-    _REV_COUNT=$(git rev-list "${PREV_TAG}"..HEAD --count 2>/dev/null) || true
-    if [[ ! -z "${_REV_COUNT:-}" ]]; then
-        VERSION="${PREV_TAG}+internal.${_REV_COUNT}"
-    fi
 fi
 
 echo "VERSION is ${VERSION}"
